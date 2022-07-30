531b8e48cf9fffab0d27b6888e22002d4687ac0e
Nancy Jain <nancyjain0631@gmail.com>
2022-07-30 12:20:00 +0530

    Initial Commit

---
+++ NaviAssignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaviAssignment1
+//
+//  Created by Nancy Jain on 30/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
